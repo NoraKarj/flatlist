bf63c55b7a85fa191485821fb92b213c1e2837c1
NoraKarj <t1kano00@students.oamk.fi>
2023-09-27 15:44:22 +0300

    Create DATA.js

---
+++ DATA.js
@@ -0,0 +1,43 @@
+export const DATA = [
+    {
+        id:'1',
+        lastname: 'Karjalainen',
+        firstname: 'Hermanni'
+    },
+    {
+        id: '2',
+        lastname: 'Karppinen',
+        firstname: 'Sohvi'
+    },
+    {
+        id:'3',
+        lastname: 'Lappalainen',
+        firstname: 'Susan'
+    },
+    {
+        id:'4',
+        lastname: 'Keränen',
+        firstname: 'Anni'
+    },
+    {
+        id:'5',
+        lastname: 'Taalasmaa',
+        firstname: 'Seppo'
+    },
+    {
+        id:'6',
+        lastname: 'Korhonen',
+        firstname: 'Veijo'
+    },
+    {
+        id:'7',
+        lastname: 'Saarinen',
+        firstname: 'Marjatta'
+    },
+    {
+        id:'8',
+        lastname: 'Loistava',
+        firstname: 'Keijo'
+    }
+
+]
